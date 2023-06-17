96455706f75db214cb54e5494847b9b6b355e906
Zeioth <zeioth@hotmail.com>
2023-06-17 07:19:51 +0200

    x

--- lua/compiler/languages/c.lua
+++ lua/compiler/languages/c.lua
@@ -7,8 +7,7 @@ M.options = {
   { text = "1 - Build and run program", value = "option1" },
   { text = "2 - Build progrm", value = "option2" },
   { text = "3 - Run program", value = "option3" },
-  { text = "4 - Build solution", value = "option4" },
-  { text = "5 - Make", value = "option5" }
+  { text = "4 - Make", value = "option5" }
 }
 
 -- Backend - overseer tasks performed on option selected
@@ -56,27 +55,7 @@ function M.action(selected_option)
         },},},})
     task:start()
     vim.cmd("OverseerOpen")
-  elseif selected_option == "option4" then -- If option 3
-    -- Search for all main.c files in the working directory, and compile them
-    -- TODO: Cuanto todo haya terminado. Abrimos progarma si tenemos su ruta
-    local entry_points = require("compiler.utils").find_files(vim.fn.getcwd(), "main.c")
-    local output_dirs = vim.fn.getcwd() .. "/bin/"        -- working_directory/bin/
-    local outputs = vim.fn.getcwd() .. "/bin/program"     -- working_directory/bin/program
-
-    local tasks = {}
-    for _ in ipairs(entry_points) do
-      tasks[_] = { "shell", name = "- Build program → " .. entry_points[_],
-        cmd = "rm -rf " .. entry_point ..                                      -- clean
-              " && mkdir -p " .. output_dir ..                                 -- mkdir
-              " && gcc " .. entry_points[_] .. " -o " .. output .. " -Wall" .. -- compile
-              " && echo '" .. final_message .. "'"                             -- echo
-      }
-    end
-    local task = overseer.new_task({
-      name = "- C compiler", strategy = { "orchestrator", tasks = tasks ,},})
-    task:start()
-    vim.cmd("OverseerOpen")
-  elseif selected_option == "option5" then -- If option 3
+  elseif selected_option == "option 4" then -- If option 3
     local task = overseer.new_task({
       name = "- C compiler",
       strategy = { "orchestrator",
