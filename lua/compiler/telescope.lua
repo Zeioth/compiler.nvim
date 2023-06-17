bb10c740db754398a3bdcef2bb1e3d9972a7604e
Zeioth <zeioth@hotmail.com>
2023-06-17 05:59:22 +0200

    x

--- lua/compiler/telescope.lua
+++ lua/compiler/telescope.lua
@@ -41,7 +41,7 @@ function M.show()
     pickers
       .new({}, {
         prompt_title = "Compiler",
-        prompt_prefix = "Actions",
+        preview_title = "Actions",
         finder = finders.new_table {
           results = options,
           entry_maker = function(entry)
