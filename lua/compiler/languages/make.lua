d39c44e3fdcf9c56f2e3da10b4af0729c416b242
Zeioth <zeioth@hotmail.com>
2023-06-23 15:13:01 +0200

    refactor(make) Now provides a helper method to avoid code repetition.

--- lua/compiler/languages/make.lua
+++ lua/compiler/languages/make.lua
@@ -1,31 +1,37 @@
 --- Make language actions
--- Supporting this filetype  allow the user
--- to run the compiler while editing a Makefile.
+-- Supporting this filetype allow the user
+-- to use the compiler while editing a Makefile.
 
 local M = {}
 
--- Frontend  - options displayed on telescope
+--- Frontend  - options displayed on telescope
 M.options = {
   { text = "1 - Run Makefile", value = "option1" }
 }
 
--- Backend - overseer tasks performed on option selected
-function M.action(selected_option)
+--- Helper
+-- Runs ./Makefile in the current working directory.
+-- This prevents code repetition as this method is meant to be called by
+-- all other languages to act as glue for border cases.
+function M.run_makefile()
   local overseer = require("overseer")
   local final_message = "--task finished--"
+  local makefile = vim.fn.getcwd() .. "/Makefile"
+  local task = overseer.new_task({
+    name = "- C compiler",
+    strategy = { "orchestrator",
+      tasks = {{ "shell", name = "- Run Makefile → " .. makefile,
+          cmd = "time make -f " .. makefile ..                               -- run
+              " ; echo '" .. final_message .. "'"                            -- echo
+      },},},})
+  task:start()
+  vim.cmd("OverseerOpen")
+end
 
+--- Backend - overseer tasks performed on option selected
+function M.action(selected_option)
   if selected_option == "option1" then
-    local makefile = vim.fn.getcwd() .. "/Makefile"
-    local task = overseer.new_task({
-      name = "- C compiler",
-      strategy = { "orchestrator",
-        tasks = {{ "shell", name = "- Run Makefile → " .. makefile,
-            cmd = "time make -f " .. makefile ..                             -- run
-                " ; echo '" .. final_message .. "'"                          -- echo
-        },},},})
-    task:start()
-    vim.cmd("OverseerOpen")
-
+    M.run_makefile()
   end
 end
 
