13f17052187ddf0130a8ce47c763aea647010c77
Zeioth <zeioth@hotmail.com>
2023-07-06 00:53:59 +0200

    fix(asm) now follow compiler.nvim naming conventions.

--- lua/compiler/languages/asm.lua
+++ lua/compiler/languages/asm.lua
@@ -13,69 +13,121 @@ M.options = {
 --- Backend - overseer tasks performed on option selected
 function M.action(selected_option)
   local utils = require("compiler.utils")
-  local actual_filename = vim.fn.expand('%:t')                               -- example.asm
-  local actual_filename_without_extension = vim.fn.expand('%:t:r')           -- example
-  local overseer = require("overseer")
-  local entry_point = vim.fn.getcwd() .. "/" .. actual_filename              -- working_directory/example.cpp
-  local output_dir = vim.fn.getcwd() .. "/"                                  -- working_directory
-  local output = vim.fn.getcwd() .. "/" .. actual_filename_without_extension -- working_directory/filename
+  local entry_point = utils.osPath(vim.fn.getcwd() .. "/main")       -- working_directory/main.asm
+  local output_dir = utils.osPath(vim.fn.getcwd() .. "/bin/")        -- working_directory/bin/
+  local output = utils.osPath(vim.fn.getcwd() .. "/bin/program")     -- working_directory/bin/program
   local final_message = "--task finished--"
-
 
   if selected_option == "option1" then
     local task = overseer.new_task({
-      name = "- nasm ",
-      strategy = {
-        "orchestrator",
-        tasks = { {
-          "shell",
-          name = "- Build & run program → " .. entry_point,
-          cmd = "rm -f " .. actual_filename_without_extension ..                 -- clean
-              " && nasm -f elf64 " .. entry_point .. " -o " .. output .. ".o" .. -- compile
-              " && ld " .. output .. ".o " .. "-o " .. output ..                 -- link
-              " && time " ..
-              "./" .. actual_filename_without_extension ..                       -- run
-              " && echo '" .. final_message .. "'"                               -- echo
-        }, },
-      },
-    })
+      name = "- Assembly compiler",
+      strategy = { "orchestrator",
+        tasks = {{ "shell", name = "- Build & run program → " .. entry_point,
+          cmd = "rm -f " .. output .. ".o && rm -f " .. output ..                     -- clean
+                " && mkdir -p " .. output_dir ..                                      -- mkdir
+                " && nasm -f elf64 " .. entry_point .. " -o " .. output .. ".o " .. parameters .. -- compile
+                " && ld " .. entry_point .. ".o -o " .. output .. " " ..              -- link
+                " && time " .. output ..                                              -- run
+                " && echo '" .. final_message .. "'"                                  -- echo
+        },},},})
     task:start()
     vim.cmd("OverseerOpen")
   elseif selected_option == "option2" then
     local task = overseer.new_task({
-      name = "- nasm ",
-      strategy = {
-        "orchestrator",
-        tasks = { {
-          "shell",
-          name = "- Build & run program → " .. entry_point,
-          cmd = "rm -f " .. actual_filename_without_extension ..                 -- clean
-              " && nasm -f elf64 " .. entry_point .. " -o " .. output .. ".o" .. -- compile
-              " && ld " .. output .. ".o " .. "-o " .. output ..                 -- link
-              " && echo '" .. final_message .. "'"                               -- echo
-        }, },
-      },
-    })
+      name = "- Assembly compiler",
+      strategy = { "orchestrator",
+        tasks = {{ "shell", name = "- Build program → " .. entry_point,
+          cmd = "rm -f " .. output .. ".o && rm -f " .. output ..                     -- clean
+                " && mkdir -p " .. output_dir ..                                      -- mkdir
+                " && nasm -f elf64 " .. entry_point .. " -o " .. output .. ".o " .. parameters .. -- compile
+                " && ld " .. entry_point .. ".o -o " .. output .. " " ..              -- linkYa lo dijo mariano:
+                " && echo '" .. final_message .. "'"                                  -- echo
+        },},},})
     task:start()
     vim.cmd("OverseerOpen")
   elseif selected_option == "option3" then
     local task = overseer.new_task({
-      name = "- nasm ",
-      strategy = {
-        "orchestrator",
-        tasks = { {
-          "shell",
-          name = "- Build & run program → " .. entry_point,
-          cmd = "time " ..
-              "./" .. actual_filename_without_extension .. -- run
-              " && echo '" .. final_message .. "'"         -- echo
-        }, },
-      },
-    })
+      name = "- Assembly compiler",
+      strategy = { "orchestrator",
+        tasks = {{ "shell", name = "- Run program → " .. entry_point,
+          cmd = "time " .. output ..                                                  -- run
+                " && echo '" .. final_message .. "'"                                  -- echo
+        },},},})
     task:start()
     vim.cmd("OverseerOpen")
   elseif selected_option == "option4" then
-    require("compiler.languages.make").run_makefile() -- run
+    local entry_points
+    local tasks = {}
+    local task
+
+    -- if .solution file exists in working dir
+    if utils.fileExists(".solution") then
+      local config = utils.parseConfigFile(
+        os_path(vim.fn.getcwd() .. "/.solution"))
+      local executable
+
+      for entry, variables in pairs(config) do
+        executable = utils.osPath(variables.executable)
+        if executable then goto continue end
+        entry_point = utils.osPath(variables.entry_point)
+        output = utils.osPath(variables.output)
+        output_dir = utils.osPath(output:match("^(.-[/\\])[^/\\]*$"))
+        parameters = variables.parameters or parameters -- optional
+        task = { "shell", name = "- Build program → " .. entry_point,
+          cmd = "rm -f " .. output .. ".o && rm -f " .. output ..                     -- clean
+                " && mkdir -p " .. output_dir ..                                      -- mkdir
+                " && nasm -f elf64 " .. entry_point .. " -o " .. output .. ".o " .. parameters .. -- compile
+                " && ld " .. entry_point .. ".o -o " .. output .. " " ..              -- link
+                " && echo '" .. final_message .. "'"                                  -- echo
+        }
+        table.insert(tasks, task) -- store all the tasks we've created
+        ::continue::
+      end
+
+      if executable then
+        task = { "shell", name = "- Run program → " .. executable,
+          cmd = "time " .. executable ..                                     -- run
+                " && echo '" .. final_message .. "'"                         -- echo
+        }
+      else
+        task = {}
+      end
+
+      task = overseer.new_task({
+        name = "- Assembly compiler", strategy = { "orchestrator",
+          tasks = {
+            tasks, -- Build all the programs in the solution in parallel
+            task   -- Then run the solution executable
+          }}})
+      task:start()
+      vim.cmd("OverseerOpen")
+
+    else -- If no .solution file
+      -- Create a list of all entry point files in the working directory
+      entry_points = utils.find_files(vim.fn.getcwd(), "main.c")
+
+      for _, ep in ipairs(entry_points) do
+        ep = utils.osPath(ep)
+        output_dir = utils.osPath(ep:match("^(.-[/\\])[^/\\]*$") .. "/bin") -- entry_point/bin
+        output = utils.osPath(output_dir .. "/program")                     -- entry_point/bin/program
+        task = { "shell", name = "- Build program → " .. ep,
+          cmd = "rm -f " .. output .. ".o && rm -f " .. output ..            -- clean
+                " && mkdir -p " .. output_dir ..                             -- mkdir
+                " && nasm -f elf64 " .. ip .. " -o " .. output .. ".o " .. parameters .. -- compile
+                " && ld " .. ip .. ".o -o " .. output .. " " ..              -- link
+                " && echo '" .. final_message .. "'"                         -- echo
+        }
+        table.insert(tasks, task) -- store all the tasks we've created
+      end
+
+      task = overseer.new_task({ -- run all tasks we've created secuentially
+        name = "- Assembly compiler", strategy = { "orchestrator", tasks = tasks }
+      })
+      task:start()
+      vim.cmd("OverseerOpen")
+    end
+  elseif selected_option == "option5" then
+    require("compiler.languages.make").run_makefile()                        -- run
   end
 end
 
