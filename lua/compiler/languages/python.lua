7bfe726fee5af47d2b6cd1fbe4f433b51aa5beb7
Zeioth <zeioth@hotmail.com>
2023-07-24 03:57:37 +0200

    fix(python) same as the former.

--- lua/compiler/languages/python.lua
+++ lua/compiler/languages/python.lua
@@ -75,7 +75,7 @@ function M.action(selected_option)
         local entry_point = utils.osPath(variables.entry_point)
         local parameters = variables.parameters or "" -- optional
         task = { "shell", name = "- Run program → " .. entry_point,
-          cmd = "python " .. entry_point .. " " .. parameters  ..            -- run (interpreted)
+          cmd = "python " .. parameters .. " " .. entry_point ..             -- run (interpreted)
                 " && echo " .. entry_point ..                                -- echo
                 " && echo '" .. final_message .. "'"
         }
@@ -97,7 +97,7 @@ function M.action(selected_option)
       for _, entry_point in ipairs(entry_points) do
         entry_point = utils.osPath(entry_point)
         task = { "shell", name = "- Build program → " .. entry_point,
-          cmd = "python " .. entry_point .. " " .. parameters  ..            -- run (interpreted)
+          cmd = "python " .. parameters .. " " .. entry_point ..             -- run (interpreted)
                 " && echo " .. entry_point ..                                -- echo
                 " && echo '" .. final_message .. "'"
         }
