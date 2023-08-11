6fe59ca22341e459a38db29f9746a3ecdb73365f
Zeioth <zeioth@hotmail.com>
2023-08-11 17:33:27 +0200

    feat(debug) we missed rust from the prev commit.

--- lua/compiler/languages/rust.lua
+++ lua/compiler/languages/rust.lua
@@ -18,7 +18,7 @@ function M.action(selected_option)
   local entry_point = utils.osPath(vim.fn.getcwd() .. "/main.rs")            -- working_directory/main.rs
   local output_dir = utils.osPath(vim.fn.getcwd() .. "/bin/")                -- working_directory/bin/
   local output = utils.osPath(vim.fn.getcwd() .. "/bin/program")             -- working_directory/bin/program
-  local parameters = "-D warnings"                                           -- parameters can be overriden in .solution
+  local parameters = "-D warnings -g"                                        -- parameters can be overriden in .solution
   local final_message = "--task finished--"
 
   if selected_option == "option1" then
