452a58ab674bd0b85e5eb06b834811e8cb0bf6f6
Zeioth <zeioth@hotmail.com>
2023-08-26 07:07:08 +0200

    chore(test suite): All new languages added and passing.

--- tests/tests.lua
+++ tests/tests.lua
@@ -12,12 +12,19 @@ dofile(tests_dir .. 'asm.lua')
 dofile(tests_dir .. 'java.lua')
 dofile(tests_dir .. 'python.lua')
 dofile(tests_dir .. 'ruby.lua')
-dofile(tests_dir .. 'sh.lua')
+dofile(tests_dir .. 'lua.lua')
+dofile(tests_dir .. 'perl.lua')
 dofile(tests_dir .. 'fsharp.lua')
+dofile(tests_dir .. 'r.lua')
+dofile(tests_dir .. 'dart.lua')
+dofile(tests_dir .. 'sh.lua')
 dofile(tests_dir .. 'make.lua')
 
 -- Cases that require to be tested manually atm.
--- * python → Run this file.
--- * ruby   → Run this file.
--- * shell  → Run this file.
--- * fsharp → Run this file.
+-- * python      → Run this file.
+-- * ruby        → Run this file.
+-- * shell       → Run this file.
+-- * fsharp      → Run this file.
+-- * r           → Run this file.
+-- * python/r/F# → REPL
+-- * dart        → Run progam (flutter).
