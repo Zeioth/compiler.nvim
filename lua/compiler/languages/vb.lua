eeea94da8bb6e2b9fd8b54dc255f09b7e6091f30
Zeioth <zeioth@hotmail.com>
2023-08-29 15:10:59 +0200

    feat(Visual Basic .NET) New language added.

---
+++ lua/compiler/languages/vb.lua
@@ -0,0 +1,42 @@
+--- Visual basic dotnet language actions
+
+local M = {}
+
+--- Frontend  - options displayed on telescope
+M.options = {
+  { text = "1 - Dotnet build and run", value = "option1" },
+  { text = "2 - Dotnet build", value = "option2" },
+  { text = "3 - Run Makefile", value = "option3" }
+}
+
+--- Backend - overseer tasks performed on option selected
+function M.action(selected_option)
+  local overseer = require("overseer")
+  local final_message = "--task finished--"
+
+  if selected_option == "option1" then
+    local task = overseer.new_task({
+      name = "- Visual basic dotnet compiler",
+      strategy = { "orchestrator",
+        tasks = {{ "shell", name = "- Dotnet build & run → .vbproj",
+          cmd = "dotnet run" ..                                                      -- compile and run
+                " && echo '" .. final_message .. "'"                                 -- echo
+        },},},})
+    task:start()
+    vim.cmd("OverseerOpen")
+  elseif selected_option == "option2" then
+    local task = overseer.new_task({
+      name = "- Visual basic dotnet compiler",
+      strategy = { "orchestrator",
+        tasks = {{ "shell", name = "- Dotnet build → .vbproj",
+          cmd = "dotnet build" ..                                                    -- compile
+                " && echo '" .. final_message .. "'"                                 -- echo
+        },},},})
+    task:start()
+    vim.cmd("OverseerOpen")
+  elseif selected_option == "option3" then
+    require("compiler.languages.make").run_makefile()                        -- run
+  end
+end
+
+return M
