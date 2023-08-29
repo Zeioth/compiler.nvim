48f0b0b8ff359a98ffc42ebb1c83869a074d2c29
Zeioth <zeioth@hotmail.com>
2023-08-29 15:10:25 +0200

    feat(examples): Visual Basic .NET

---
+++ tests/examples/vb/build-and-run-dotnet/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module MainModule
+    Sub Main()
+        Dim helper As New Helper()
+        helper.SayHello()
+    End Sub
+End Module
+
---
+++ tests/examples/vb/build-dotnet/Helper.vb
@@ -0,0 +1,8 @@
+Imports System
+
+Public Class Helper
+    Public Sub SayHello()
+        Console.WriteLine("Hello, world!")
+    End Sub
+End Class
+
