3ca5e8ff33ac74b4c6e40022668ffd190705cc34
Zeioth <zeioth@hotmail.com>
2023-08-30 10:22:37 +0200

    feat(zig): New language added

---
+++ lua/compiler/languages/zig.lua
@@ -0,0 +1,46 @@
+--- Zig language actions
+--- Note: You must initialize your project with:
+--  'zig init-exe' or 'zig init-lib'
+--- to use the compiler options defined here.
+
+local M = {}
+
+--- Frontend  - options displayed on telescope
+M.options = {
+  { text = "1 - Zig build run", value = "option1" },
+  { text = "2 - Zig build", value = "option2" },
+  { text = "3 - Run Makefile", value = "option3" }
+}
+
+--- Backend - overseer tasks performed on option selected
+function M.action(selected_option)
+  local overseer = require("overseer")
+  local arguments = ""
+  local final_message = "--task finished--"
+
+  if selected_option == "option1" then
+    local task = overseer.new_task({
+      name = "- Zig compiler",
+      strategy = { "orchestrator",
+        tasks = {{ "shell", name = "- Build & run program → build.zig",
+          cmd = "zig build run " .. arguments ..                             -- compile and run
+                " && echo '" .. final_message .. "'"
+        },},},})
+    task:start()
+    vim.cmd("OverseerOpen")
+  elseif selected_option == "option2" then
+    local task = overseer.new_task({
+      name = "- Zig compiler",
+      strategy = { "orchestrator",
+        tasks = {{ "shell", name = "- Build program → build.zig",
+          cmd = "zig build " .. arguments ..                                 -- compile
+                " && echo '" .. final_message .. "'"
+        },},},})
+    task:start()
+    vim.cmd("OverseerOpen")
+  elseif selected_option == "option3" then
+    require("compiler.languages.make").run_makefile()                        -- run
+  end
+end
+
+return M
