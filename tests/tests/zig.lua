f536507dbd5931554318c21e9b0310b10215d9bf
Zeioth <zeioth@hotmail.com>
2023-08-30 10:21:52 +0200

    feat(tests): zig

---
+++ tests/tests/zig.lua
@@ -0,0 +1,16 @@
+--- This test file run all supported cases of use.
+--- @usage :luafile ~/.local/share/nvim/lazy/compiler.nvim/tests/tests/zig.lua
+
+local ms = 1000 -- wait time
+local language = require("compiler.languages.zig")
+local example = vim.fn.stdpath "data" .. "/lazy/compiler.nvim/tests/examples/zig/"
+
+-- Build and run
+vim.api.nvim_set_current_dir(example .. "build-and-run/")
+language.action("option1")
+vim.wait(ms)
+
+-- Build
+vim.api.nvim_set_current_dir(example .. "build/")
+language.action("option2")
+vim.wait(ms)
