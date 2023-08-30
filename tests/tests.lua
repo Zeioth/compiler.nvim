ba1e95790714fd141d10eda2c8556e5647d4083c
Zeioth <zeioth@hotmail.com>
2023-08-30 16:35:27 +0200

    chore(tests) Are now ordered alphabetically.

--- tests/tests.lua
+++ tests/tests.lua
@@ -4,25 +4,27 @@
  -- path of this script → tests
 local tests_dir = (debug.getinfo(1, 'S').source:sub(2):match '(.*/)') .. "/tests/"
 
+dofile(tests_dir .. 'asm.lua')
 dofile(tests_dir .. 'c.lua')
 dofile(tests_dir .. 'cpp.lua')
 dofile(tests_dir .. 'cs.lua')
-dofile(tests_dir .. 'rust.lua')
-dofile(tests_dir .. 'zig.lua')
-dofile(tests_dir .. 'asm.lua')
-dofile(tests_dir .. 'java.lua')
-dofile(tests_dir .. 'python.lua')
-dofile(tests_dir .. 'ruby.lua')
-dofile(tests_dir .. 'lua.lua')
-dofile(tests_dir .. 'perl.lua')
-dofile(tests_dir .. 'fsharp.lua')
-dofile(tests_dir .. 'r.lua')
 dofile(tests_dir .. 'dart.lua')
-dofile(tests_dir .. 'kotlin.lua')
-dofile(tests_dir .. 'typescript.lua')
+dofile(tests_dir .. 'fsharp.lua')
+dofile(tests_dir .. 'go.lua')
+dofile(tests_dir .. 'java.lua')
 dofile(tests_dir .. 'javascript.lua')
-dofile(tests_dir .. 'sh.lua')
+dofile(tests_dir .. 'kotlin.lua')
+dofile(tests_dir .. 'lua.lua')
 dofile(tests_dir .. 'make.lua')
+dofile(tests_dir .. 'perl.lua')
+dofile(tests_dir .. 'python.lua')
+dofile(tests_dir .. 'r.lua')
+dofile(tests_dir .. 'ruby.lua')
+dofile(tests_dir .. 'rust.lua')
+dofile(tests_dir .. 'sh.lua')
+dofile(tests_dir .. 'typescript.lua')
+dofile(tests_dir .. 'vb.lua')
+dofile(tests_dir .. 'zig.lua')
 
 -- Cases that require to be tested manually atm.
 -- * python             → Run this file.
@@ -30,6 +32,9 @@ dofile(tests_dir .. 'make.lua')
 -- * shell              → Run this file.
 -- * fsharp             → Run this file.
 -- * r                  → Run this file.
+-- * typescript         → Run this file.
+-- * javascript         → Run this file.
 -- * python/r/F#/kotlin → REPL
 -- * dart               → Run program (flutter).
 -- * kotlin             → Android studio adb.
+
