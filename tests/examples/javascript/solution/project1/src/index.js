d31ce1eab069dd54241e027d2ac70132768e222a
Zeioth <zeioth@hotmail.com>
2023-08-30 16:11:50 +0200

    feat(examples): Javascript

---
+++ tests/examples/javascript/solution/project1/src/index.js
@@ -0,0 +1,6 @@
+// index.js
+const { sayHello } = require('./helper');
+
+const greeting = sayHello('World');
+console.log(greeting);
+
---
+++ tests/examples/javascript/solution/project2/src/helper.js
@@ -0,0 +1,7 @@
+// helper.js
+function sayHello(name) {
+  return `Hello, ${name}!`;
+}
+
+module.exports = { sayHello };
+
