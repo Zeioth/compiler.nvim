51211797d7114deb11a51328a71cbe73b6b8bf07
Ben <bennette.benoy@outlook.com>
2023-10-18 16:05:22 +1000

    add tsc and ts-node to checkhealth

--- lua/compiler/health.lua
+++ lua/compiler/health.lua
@@ -18,7 +18,7 @@ function M.check()
 
   health.info(
     "Neovim Version: v"
-      .. vim.fn.matchstr(vim.fn.execute "version", "NVIM v\\zs[^\n]*")
+    .. vim.fn.matchstr(vim.fn.execute "version", "NVIM v\\zs[^\n]*")
   )
 
   if vim.version().prerelease then
@@ -95,6 +95,16 @@ function M.check()
       type = "warn",
       msg = "Used to call the make interpreter.",
     },
+    {
+      cmd = "tsc",
+      type = "warn",
+      mag = "Used to call the typescript compiler.",
+    },
+    {
+      cmd = "ts-node",
+      type = "warn",
+      msg = "Used to call the typescript interpreter.",
+    }
   }
 
   for _, program in ipairs(programs) do
@@ -120,4 +130,3 @@ function M.check()
 end
 
 return M
-
