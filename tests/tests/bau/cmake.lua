0e340fc96502fc79f24ebb629a63e68c3ebfd4c4
Zeioth <zeioth@hotmail.com>
2023-10-31 00:48:26 +0100

    feat(tests): Cmake

--- tests/tests/bau/cmake.lua
+++ tests/tests/bau/cmake.lua
@@ -2,10 +2,10 @@
 --- @usage :luafile ~/.local/share/nvim/lazy/compiler.nvim/tests/tests/bau/make.lua
 
 local ms = 1000 -- wait time
-local language = require("compiler.bau.make")
-local example = vim.fn.stdpath "data" .. "/lazy/compiler.nvim/tests/examples/bau/make"
+local language = require("compiler.bau.cmake")
+local example = vim.fn.stdpath "data" .. "/lazy/compiler.nvim/tests/examples/bau/cmake"
 
--- Run makefile
+-- Run CMakeLists.txt
 vim.api.nvim_set_current_dir(example)
 language.action("hello_world")
 vim.wait(ms)
