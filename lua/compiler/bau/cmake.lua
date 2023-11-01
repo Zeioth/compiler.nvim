7d7867769b3923288e1339c32f4c62fbb57848dc
Zeioth <zeioth@hotmail.com>
2023-11-01 23:12:00 +0100

    feat(cmake): Will now use the globals `CMAKE_BUILD_DIR` and `CMAKE_BUILD_TYPE` when defined.

--- lua/compiler/bau/cmake.lua
+++ lua/compiler/bau/cmake.lua
@@ -2,19 +2,31 @@
 
 local M = {}
 
+
 -- Backend - overseer tasks performed on option selected
 function M.action(option)
   local overseer = require("overseer")
   local final_message = "--task finished--"
+
+  -- Global: CMAKE_BUILD_DIR
+  local build_dir = vim.api.nvim_get_var("CMAKE_BUILD_DIR") or "./build"
+  vim.api.nvim_set_var("CMAKE_BUILD_DIR", build_dir)
+
+  -- Global: CMAKE_BUILD_TYPE
+  local build_type = vim.api.nvim_get_var("CMAKE_BUILD_TYPE") or "Release"
+  vim.api.nvim_set_var("CMAKE_BUILD_TYPE", build_type)
+
+  -- Run command
+  local cmd_build = "cmake -S . -B " .. build_dir .. " -DCMAKE_BUILD_TYPE=" .. build_type
+  local cmd_target = "cmake --build " .. build_dir .. " --target " .. option
   local task = overseer.new_task({
     name = "- CMake interpreter",
     strategy = { "orchestrator",
-      tasks = {{ "shell", name = "- Run CMake → cmake -S . -B build &&  cmake --build build --target " .. option,
-        cmd = "rm -rf ./build" ..
-              " && mkdir -p ./build" ..
-              " && cmake -S . -B build" ..                                   -- Generate build files in a 'build' directory
-              " && cmake --build build --target " .. option ..               -- Build the specified target from the 'build' directory
-              " && echo cmake " .. option ..                                 -- echo
+      tasks = {{ "shell", name = "- Run CMake → " .. option,
+        cmd = "mkdir -p " .. build_dir ..
+              " && " .. cmd_build ..                                         -- Build to 'build' directory.
+              " && " .. cmd_target ..                                        -- Build target from the 'build' directory.
+              " && echo '" .. cmd_build .. " && " .. cmd_target .. "'" ..    -- echo
               " && echo '" .. final_message .. "'"
       },},},})
   task:start()
