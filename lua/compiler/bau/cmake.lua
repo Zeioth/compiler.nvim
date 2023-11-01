52479b94e55160857aaf5e0efc71d5f80a3277e4
Zeioth <zeioth@hotmail.com>
2023-11-02 00:57:02 +0100

    fix(cmake): error control on undefined global.

--- lua/compiler/bau/cmake.lua
+++ lua/compiler/bau/cmake.lua
@@ -2,23 +2,29 @@
 
 local M = {}
 
-
 -- Backend - overseer tasks performed on option selected
 function M.action(option)
   local overseer = require("overseer")
   local final_message = "--task finished--"
 
   -- Global: CMAKE_BUILD_DIR
-  local build_dir = vim.api.nvim_get_var("CMAKE_BUILD_DIR") or "./build"
-  vim.api.nvim_set_var("CMAKE_BUILD_DIR", build_dir)
+  local build_dir = vim.api.nvim_get_var('CMAKE_BUILD_DIR')
+  if not build_dir then build_dir = './build' end
 
   -- Global: CMAKE_BUILD_TYPE
-  local build_type = vim.api.nvim_get_var("CMAKE_BUILD_TYPE") or "Debug"
-  vim.api.nvim_set_var("CMAKE_BUILD_TYPE", build_type)
+  local build_type = vim.api.nvim_get_var('CMAKE_BUILD_TYPE')
+  if not build_type then build_dir = 'debug' end
+
+  -- Global: CMAKE_CLEAN_FIRST
+  local clean_first_arg = ""
+  local clean_first = vim.api.nvim_get_var('CMAKE_CLEAN_FIRST')
+  if clean_first and clean_first == "true" then
+    clean_first_arg = "--clean-first"
+  end
 
   -- Run command
   local cmd_build = "cmake -S . -B " .. build_dir .. " -DCMAKE_BUILD_TYPE=" .. build_type
-  local cmd_target = "cmake --build " .. build_dir .. " --target " .. option
+  local cmd_target = "cmake --build " .. build_dir .. " --target " .. option .. " " .. clean_first_arg
   local task = overseer.new_task({
     name = "- CMake interpreter",
     strategy = { "orchestrator",
