5bc0e1c5848eca7892de7c3b7b744ba0607eb495
Zeioth <zeioth@hotmail.com>
2023-11-13 23:21:43 +0100

    ✨feat(gradle): Examples added.

---
+++ tests/examples/bau/gradle/build.gradle.kts
@@ -0,0 +1,14 @@
+// NOTE: build.gradle.kts will prevail over build.gradle.
+
+// Example of a task
+tasks.register("hello_world") {
+    doLast {
+        println("Hello, World!")
+    }
+}
+
+// Example of a custom task to run all tasks
+tasks.register("build_all") {
+    dependsOn("hello_world")
+}
+
