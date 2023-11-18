eca97b4a83df96b2c7423bd22e558074541a35c2
Zeioth <zeioth@hotmail.com>
2023-11-18 02:39:55 +0100

    ✅test(examples): javascript and typescript npm tests have been moved to bau packagejs.

--- tests/examples/languages/javascript/npm/src/index.js
+++
@@ -1,7 +0,0 @@
-import { getGreeting } from "./helper.js";
-function sayHello() {
-    const greeting = getGreeting();
-    const name = "World";
-    console.log(`${greeting}, ${name}!`);
-}
-sayHello();
