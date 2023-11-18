b7a3a01f8404a9d1104c5c96099a97e749491b1b
Zeioth <zeioth@hotmail.com>
2023-11-18 02:42:38 +0100

    test(bau) test notation changed from `language` to `bau` for bau.

--- tests/tests/bau/gradle.lua
+++ tests/tests/bau/gradle.lua
@@ -2,10 +2,10 @@
 --- @usage :luafile ~/.local/share/nvim/lazy/compiler.nvim/tests/tests/bau/gradle.lua
 
 local ms = 1000 -- wait time
-local language = require("compiler.bau.gradle")
+local bau = require("compiler.bau.gradle")
 local example = vim.fn.stdpath "data" .. "/lazy/compiler.nvim/tests/examples/bau/gradle"
 
 -- Run gradlew > build.gradlew.kts > build.gradlew (first occurence by priority)
 vim.api.nvim_set_current_dir(example)
-language.action("hello_world")
+bau.action("hello_world")
 vim.wait(ms)
--- tests/tests/bau/make.lua
+++ tests/tests/bau/make.lua
@@ -2,10 +2,10 @@
 --- @usage :luafile ~/.local/share/nvim/lazy/compiler.nvim/tests/tests/bau/make.lua
 
 local ms = 1000 -- wait time
-local language = require("compiler.bau.make")
+local bau = require("compiler.bau.make")
 local example = vim.fn.stdpath "data" .. "/lazy/compiler.nvim/tests/examples/bau/make"
 
 -- Run makefile
 vim.api.nvim_set_current_dir(example)
-language.action("hello_world")
+bau.action("hello_world")
 vim.wait(ms)
