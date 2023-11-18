23253e0abbba866bb93825d648d4ad0016fb64a2
Zeioth <zeioth@hotmail.com>
2023-11-18 02:41:57 +0100

    ✅test(package.js) added

---
+++ tests/tests/bau/packagejs.lua
@@ -0,0 +1,20 @@
+--- This test file run all supported cases of use.
+--- @usage :luafile ~/.local/share/nvim/lazy/compiler.nvim/tests/tests/bau/packagejs.lua
+
+local bau = require("compiler.bau.packagejs")
+
+-- Run nodejs javascript project
+local example = vim.fn.stdpath "data" .. "/lazy/compiler.nvim/tests/examples/bau/packagejs/example-javascript"
+vim.api.nvim_set_current_dir(example)
+bau.action("npm install")
+vim.wait(10000) -- wait time
+bau.action("npm start")
+vim.wait(1000) -- wait time
+
+-- Run nodejs typescript project
+local example = vim.fn.stdpath "data" .. "/lazy/compiler.nvim/tests/examples/bau/packagejs/example-typescript"
+vim.api.nvim_set_current_dir(example)
+bau.action("npm install")
+vim.wait(10000) -- wait time
+bau.action("npm start")
+vim.wait(1000) -- wait time
