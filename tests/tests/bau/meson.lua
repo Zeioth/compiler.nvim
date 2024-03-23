6af4c00c1cce77a709c08a1289c8df58332b5d7b
Zeioth <zeioth@hotmail.com>
2024-03-23 03:51:11 +0100

    ✨️feat(`meson`): tests

---
+++ tests/tests/bau/meson.lua
@@ -0,0 +1,11 @@
+--- This test file run all supported cases of use.
+--- @usage :luafile ~/.local/share/nvim/lazy/compiler.nvim/tests/tests/bau/meson.lua
+
+local ms = 1000 -- wait time
+local bau = require("compiler.bau.cmake")
+local example = vim.fn.stdpath "data" .. "/lazy/compiler.nvim/tests/examples/bau/meson"
+
+-- Run meson.build
+vim.api.nvim_set_current_dir(example)
+bau.action("hello_world")
+vim.wait(ms)
