43e316b03d5576977a4b3289b1e124c6e99dfce1
Zeioth <zeioth@hotmail.com>
2024-05-24 18:41:32 +0200

    break!: Dropping deprecated nvim 0.9 functions. `:checkhealth` now requires `nvim 0.10`. No more breaking changes are expected.

--- lua/compiler/health.lua
+++ lua/compiler/health.lua
@@ -1,33 +1,23 @@
-
 -- On neovim you can run
 -- :checkhealth compiler
 -- To know possible causes in case compiler.nvim is not working correctly.
 
 local M = {}
 
--- TODO: remove deprecated method check after dropping support for neovim v0.9
-local health = {
-  start = vim.health.start or vim.health.report_start,
-  ok = vim.health.ok or vim.health.report_ok,
-  warn = vim.health.warn or vim.health.report_warn,
-  error = vim.health.error or vim.health.report_error,
-  info = vim.health.info or vim.health.report_info,
-}
-
 function M.check()
-  health.start "compiler.nvim"
+  vim.health.start("compiler.nvim")
 
-  health.info(
+  vim.health.info(
     "Neovim Version: v"
       .. vim.fn.matchstr(vim.fn.execute "version", "NVIM v\\zs[^\n]*")
   )
 
   if vim.version().prerelease then
-    health.warn "Neovim nightly is not officially supported and may have breaking changes."
-  elseif vim.fn.has "nvim-0.8" == 1 then
-    health.ok "Using stable Neovim >= 0.8.0"
+    vim.health.warn "Neovim nightly is not officially supported and may have breaking changes."
+  elseif vim.fn.has("nvim-0.10" == 1) then
+    vim.health.ok("Using stable Neovim >= 0.10.0")
   else
-    health.error "Neovim >= 0.8.0 is required"
+    vim.health.error("Neovim >= 0.10.0 is required")
   end
 
   local programs = {
@@ -186,9 +176,9 @@ function M.check()
     end
 
     if found then
-      health.ok(("`%s` is installed: %s"):format(name, program.msg))
+      vim.health.ok(("`%s` is installed: %s"):format(name, program.msg))
     else
-      health[program.type](
+      vim.health[program.type](
         ("`%s` is not installed: %s"):format(name, program.msg)
       )
     end
