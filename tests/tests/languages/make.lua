57e1487ec8ca0eea7701cb23866bc56ef4d6c127
Zeioth <zeioth@hotmail.com>
2024-06-02 14:09:40 +0200

    ✅test(make): fixed

--- tests/tests/languages/make.lua
+++ tests/tests/languages/make.lua
@@ -3,7 +3,7 @@
 
 local ms = 1000 -- wait time
 local language = require("compiler.languages.make")
-local example = vim.fn.stdpath "data" .. "/lazy/compiler.nvim/tests/examples"
+local example = vim.fn.stdpath "data" .. "/lazy/compiler.nvim/tests/examples/languages"
 
 -- Run makefile
 vim.api.nvim_set_current_dir(example)
