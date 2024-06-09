57ca6fbc9254585b217e4c6ad153978d47b55abc
Zeioth <zeioth@hotmail.com>
2024-06-09 18:12:32 +0200

    ✅tests(gradle): test for spring added.

--- tests/tests/bau/gradle.lua
+++ tests/tests/bau/gradle.lua
@@ -3,9 +3,15 @@
 
 local ms = 1000 -- wait time
 local bau = require("compiler.bau.gradle")
-local example = vim.fn.stdpath "data" .. "/lazy/compiler.nvim/tests/code samples/bau/gradle"
 
 -- Run gradlew > build.gradlew.kts > build.gradlew (first occurence by priority)
+local example = vim.fn.stdpath "data" .. "/lazy/compiler.nvim/tests/code samples/bau/gradle/parse-file"
 vim.api.nvim_set_current_dir(example)
 bau.action("hello_world")
+vim.wait(ms)
+
+-- Run gradlew > build.gradlew.kts > build.gradlew (first occurence by priority)
+local example = vim.fn.stdpath "data" .. "/lazy/compiler.nvim/tests/code samples/bau/gradle/parse-cmd"
+vim.api.nvim_set_current_dir(example)
+bau.action("bootRun")
 vim.wait(ms)
